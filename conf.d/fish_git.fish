d1c9fc84b3a167299ffe8d2e8aaf08acdbff4d57
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-07-31 09:26:55 +1000

    updated to new subcmd std

--- conf.d/fish_git.fish
+++ conf.d/fish_git.fish
@@ -1,10 +1,10 @@
 
-fd2_define_command fdg "fishdots plugin for doing useful things with git"
-fd2_define_subcommand fdg save __fd2_fdg_save "save all edits"
-fd2_define_subcommand fdg sync __fd2_fdg_sync "save all edits then push to origin"
-fd2_define_subcommand fdg release __fd2_fdg_release "(<ver> <msg>) Update release version, tag and push"
-fd2_define_subcommand fdg tag __fd2_fdg_tag "(<ver> <msg>) release, without use of latest-release.txt file"
-fd2_define_subcommand fdg ed __fd2_fdg_ed "find a file in the current repo and edit"
+fd2_define_command -p fdg "fishdots plugin for doing useful things with git"
+fd2_define_subcommand -p fdg -c save -f __fd2_fdg_save -d "save all edits"
+fd2_define_subcommand -p fdg -c sync -f __fd2_fdg_sync -d "save all edits then push to origin"
+fd2_define_subcommand -p fdg -c release -f __fd2_fdg_release -d "(<ver> <msg>) Update release version, tag and push"
+fd2_define_subcommand -p fdg -c tag -f __fd2_fdg_tag -d "(<ver> <msg>) release, without use of latest-release.txt file"
+fd2_define_subcommand -p fdg -c ed -f __fd2_fdg_ed -d "find a file in the current repo and edit"
 
 
 set -x myplugin_prefix "hello world: "
