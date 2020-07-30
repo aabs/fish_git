f9d5b0d69328e17604e34d0fb763dc88cc8a946f
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-07-31 09:46:49 +1000

    wip

--- conf.d/fish_git.fish
+++ conf.d/fish_git.fish
@@ -1,5 +1,5 @@
 
-fd2_define_command -p fdg "fishdots plugin for doing useful things with git"
+fd2_define_command -p fdg -d "fishdots plugin for doing useful things with git"
 fd2_define_subcommand -p fdg -c save -f __fd2_fdg_save -d "save all edits"
 fd2_define_subcommand -p fdg -c sync -f __fd2_fdg_sync -d "save all edits then push to origin"
 fd2_define_subcommand -p fdg -c release -f __fd2_fdg_release -d "(<ver> <msg>) Update release version, tag and push"
